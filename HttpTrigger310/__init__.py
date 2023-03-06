16039f2ab32cb96639d1a2d318275eca64842c02
Tu1234-lang <tianangn@gmail.com>
2023-03-06 16:15:45 -0500

    add backend files py

--- HttpTrigger310/__init__.py
+++ HttpTrigger310/__init__.py
@@ -2,33 +2,29 @@ import json
 import logging
 import azure.functions as func
 import socket
-VISITOR_ID = "0"
-count = 0
+VISITOR_ID = "1"
 
-def main(req: func.HttpRequest, message: func.Out[str]) -> func.HttpResponse:
+def main(req: func.HttpRequest, messageIn, message: func.Out[str]) -> func.HttpResponse:
     #get hostname and ip adress of user
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
-    #define the entity - data to be insert into the table
-    my_entity = {
-        'PartitionKey': VISITOR_ID,
-        'RowKey': ip_address,
-        'Hostname': hostname
-    }
+    #get entities from table
+    from_entity = json.loads(messageIn)
+    #if table does not have any entity, inserts a brand new one starting with 1
+    if len(from_entity) == 0:
+        my_entity = {
+            'PartitionKey': VISITOR_ID,
+            'RowKey': ip_address,
+        }
+    #if table already has an entity, takes the last PartitionKey number, and add up
+    else:
+        lastentity = (from_entity[-1]['PartitionKey'])
+        my_entity = {
+            'PartitionKey': str(int(lastentity) + 1),
+            'RowKey': ip_address,
+        }
     #insert entity
     message.set(json.dumps(my_entity))
-    #update visitor num
-    updateVISITOR()
     #get HTTP respone
     logging.info('Python HTTP trigger function processed a request.')
-    outMessage = hostname + " " + ip_address #req.get_body()
-    return func.HttpResponse(f"Received the message: {outMessage}", status_code=200)
-
-def updateVISITOR():
-    #update count
-    global count 
-    count += 1
-    #update visitor id so it matches count
-    global VISITOR_ID
-    VISITOR_ID = str(count)
-    str(VISITOR_ID)
+    return func.HttpResponse(json.dumps(my_entity),mimetype="application/json", status_code=200)
